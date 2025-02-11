52082853e37a3ec4e2c6ec33b59cd19239bdc433
alisonsiemen <alisonsiemen@oakland.edu>
2025-02-11 14:19:15 -0500

    Assignment 6

---
+++ Animal.java
@@ -0,0 +1,22 @@
+
+public  class Animal {
+    public int age;
+    public String gender;
+
+    public void isMammal() {
+        System.out.println("This is public method isMammal from class Animal");
+    }
+    public void mate() {
+        System.out.println("This is public method mate from class Animal");
+    }
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Fish myFish = new Fish();
+        Zebra myZebra = new Zebra();
+
+        myAnimal.isMammal();
+        myAnimal.mate();
+        myZebra.run();
+    }
+}
